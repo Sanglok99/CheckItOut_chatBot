0e3152db4f7aae6dbc6101b5ac4123490f35bd6e
Sanglok99 <contactlogi@gmail.com>
2024-11-17 16:17:25 +0900

    test_server update

--- test/test_server.py
+++ test/test_server.py
@@ -17,7 +17,7 @@ except: print("텍스트 전처리기 로드 실패..")
 
 #엑셀 파일 로드
 try:
-    df = pd.read_csv('train_tools/qna/question_with_labels_100.csv')
+    df = pd.read_csv('../train_tools/qna/question_with_labels_100.csv')
     print("엑셀 파일 로드 완료..")
 except: print("엑셀 파일 로드 실패..")
 
@@ -25,7 +25,7 @@ except: print("엑셀 파일 로드 실패..")
 try:
     create_embedding_data = create_embedding_data(df=df, preprocess=p)
     create_embedding_data.create_pt_file()
-    embedding_data = torch.load('train_tools/qna/embedding_data.pt')
+    embedding_data = torch.load('../train_tools/qna/embedding_data.pt')
     print("임베딩 pt 파일 갱신 및 로드 완료..")
 except: print("임베딩 pt 파일 갱신 및 로드 실패..")
 
