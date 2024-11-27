4e7bd1a6daaa7c76b89f1f51deeb79e334ed38f7
Sanglok99 <contactlogi@gmail.com>
2024-11-27 22:40:34 +0900

    add send_ping

--- server.py
+++ server.py
@@ -28,28 +28,47 @@ try:
 except: print("임베딩 pt 파일 갱신 및 로드 실패..")
 
 
-async def handler(websocket):
-    async for message in websocket:
-        ### message의 score 구해야 함. "안녕하세요", "~~~ 책 추천해줘" 와 같은 말과 유사도 비교하여 0.6 이하라면 "죄송합니다. 아직 준비되지 않은 답변입니다." 출력
-        f = FindAnswer(df=df, embedding_data=embedding_data ,preprocess=p)
-        selected_qes, score, answer = f.search(message)
-        if score < 0.6: 
-            response = "죄송합니다. 아직 준비되지 않은 답변입니다."
-        else:
-            print(selected_qes) ## 결과 출력 시 출력이 어떤 데이터에 기초했는지 확인하는 용도
-            response = answer
+# 텍스트 기반 Ping 메시지 전송
+async def send_ping(websocket):
+    try:
+        while True:
+            await websocket.send("ping")  # 텍스트 기반 ping 전송
+            print("Ping 전송")
+            await asyncio.sleep(30)  # 30초마다 Ping 전송
+    except websockets.ConnectionClosed:
+        print("클라이언트와의 연결이 닫혔습니다.")
+    except Exception as e:
+        print(f"Ping 전송 중 오류 발생: {e}")
 
-        await websocket.send(response)
+async def handler(websocket):
+    ping_task = asyncio.create_task(send_ping(websocket))  # Ping 작업 실행
+    try:
+        async for message in websocket:
+            if message == 'pong':
+                print()
+            ### message의 score 구해야 함. "안녕하세요", "~~~ 책 추천해줘" 와 같은 말과 유사도 비교하여 0.6 이하라면 "죄송합니다. 아직 준비되지 않은 답변입니다." 출력
+            f = FindAnswer(df=df, embedding_data=embedding_data ,preprocess=p)
+            selected_qes, score, answer = f.search(message)
+            if score < 0.5: ### 유사도 0.5로 수정!!! ###
+                response = "죄송합니다. 아직 준비되지 않은 답변입니다."
+            else:
+                print(selected_qes) ## 결과 출력 시 출력이 어떤 데이터에 기초했는지 확인하는 용도
+                response = answer
+
+            await websocket.send(response)
+    except websockets.ConnectionClosedError as e:
+        print(f"연결 종료: {e}")
+    except Exception as e:
+        print(f"오류 발생: {e}")
+    finally:
+        ping_task.cancel()  # Ping 태스크 중지
+        print("Ping 전송 태스크 중지")
+        await websocket.close()
+        print("WebSocket 연결이 닫혔습니다.")
 
 async def main():
-    async with websockets.serve(handler, 
-                                "0.0.0.0", 
-                                8765, 
-                                ping_interval=30,  # Ping 메시지 간격 (초)
-                                ping_timeout=60    # Ping 응답 대기 시간 (초)
-    ):
+    async with websockets.serve(handler, "0.0.0.0", 8765):
         print("서버가 ws://localhost:8765 에서 시작되었습니다.")
         await asyncio.Future()  # 서버가 계속 실행되도록 유지
 
 asyncio.run(main())
-
